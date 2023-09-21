2d254489179346faa4e9ca73a92cc8193d0c8bee
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-09-21 01:26:56 -0400

    Initial Commit

---
+++ Profs of NEU/Profs_of_NEUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profs_of_NEUApp.swift
+//  Profs of NEU
+//
+//  Created by Gaurav Bhambhani on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Profs_of_NEUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
